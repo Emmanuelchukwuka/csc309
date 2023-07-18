7f14b9a600dc770381e543d312eb72d34f2f7eca
Emmanuelchukwuka <139468186+Emmanuelchukwuka@users.noreply.github.com>
2023-07-18 08:45:34 -0400

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,8 +1,7 @@
-
 <?php
     $server = 'localhost'; // 127.0.0.1
     $username = 'root';
-    $password = 'rootroot';
+    $password = '';
     $db = 'csc309';
 
     // Open a new connection
@@ -14,8 +13,8 @@
     }
 
     // Create a Table
-    $sql = "INSERT INTO users (firstname, lastname, email) 
-    VALUES ('Nwafor', 'Benjamin', 'benjaminn@gmail.com')";
+    $sql = "INSERT INTO users (firstname, lastname, email, gender, date_of_birth) 
+    VALUES ('EMMANUEL', 'OSHINE', emmanuelchukwuka50@gmail.com', 'M', '15-02-2000')";
 
     $result = $con->query($sql);
 
